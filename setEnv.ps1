30a5daf544cbf6e9a183497dbf6e17895f2c9cb8
Peter Vranken <Peter_Vranken@Yahoo.de>
2019-07-01 23:07:41 +0200

    Configuration of location of UNIX tools for the Windows build platform

--- setEnv.ps1
+++ setEnv.ps1
@@ -10,6 +10,23 @@ function endWithError { write-host $usage; exit; }
 # Limit the allowed number of parameters.
 #if($args[0] -match '.') {endWithError}
 
+# The path to the installation of the Windows port of the unix shell tools. Note, this
+# environment may already be persistently set by the installation procedure.
+if ("$env:UNIX_TOOLS_HOME" -eq "")
+{
+    # TODO Adjust the path to the root folder of the installation of the UNIX tools (e.g. cygwin) and remove this comment and the next two lines
+    write-host ("setEnv.ps1: You need to configure this script prior to first use: Specify location of UNIX tools for Windows")
+    Write-Host "Press any key to continue ..."; $null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+    $env:UNIX_TOOLS_HOME = "C:\ProgramFiles\cygwin"
+    write-host ('Environment variable UNIX_TOOLS_HOME is set to ' + $env:UNIX_TOOLS_HOME)
+}
+else
+{
+    write-host ('Environment variable UNIX_TOOLS_HOME is already persistently set to ' `
+                + $env:UNIX_TOOLS_HOME)
+}
+
 # The path to the GCC C cross compiler installation. Note, this environment may
 # already be persistently set by the installation procedure.
 if ("$env:GCC_POWERPC_HOME" -eq "")
@@ -27,6 +44,7 @@ else
                 + $env:GCC_POWERPC_HOME)
 }
 
+
 # Create a convenience shortcut to the right instance of the make processor.
 set-alias -Name "make" `
           -Value ($env:GCC_POWERPC_HOME+"\bin\make.exe") `
@@ -34,4 +52,5 @@ set-alias -Name "make" `
 
 # Prepare the Windows search path for the run of the compilation tools.
 $env:PATH = $env:GCC_POWERPC_HOME + "\bin" `
+            + ";" + $env:UNIX_TOOLS_HOME + "\bin" `
             + ";" + $env:PATH
